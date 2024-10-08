0e2d8c707b37a43c2f7bc647e07787b8def54bcf
Izaan Saleem <sizaan55@gmail.com>
2024-10-08 15:58:19 +0500

    Day 68/100 - Switching view states with enums

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,22 +7,56 @@
 
 import SwiftUI
 
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.yellow, .gray], startPoint: .topLeading, endPoint: .bottomTrailing)
+            Text("Loading...")
+                .font(.largeTitle)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.green, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
+            Text("Success!")
+                .font(.largeTitle)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.red, .pink], startPoint: .topLeading, endPoint: .bottomTrailing)
+            Text("Failed.")
+                .font(.largeTitle)
+        }
+        .ignoresSafeArea()
+    }
+}
+
 struct ContentView: View {
+    enum LoadingState {
+        case loading
+        case success
+        case failed
+    }
+    
+    @State var loadingState: LoadingState = .success
     
     var body: some View {
-        VStack {
-            Button("Read and Write") {
-                let data = Data("Test Message".utf8)
-                let url = URL.documentsDirectory.appending(path: "message.txt")
-                
-                do {
-                    try data.write(to: url, options: [.atomic, .completeFileProtection])
-                    let input = try String(contentsOf: url, encoding: .utf8)
-                    print(input)
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
         }
     }
 }
